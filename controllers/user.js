5b521aa662f8cba98cd8860d7f024aaf93d154c8
Vaibhav <vbrana2014@gmail.com>
2023-06-07 15:01:26 +0530

    fixed sameSite

--- controllers/user.js
+++ controllers/user.js
@@ -53,7 +53,7 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {   
     res.status(200).cookie("token", null, {
-        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        sameSite: process.env.NODE_ENV === "Developement" ? "lax" : "none",
         secure: process.env.NODE_ENV === "Developement" ? false : true,
         expires: new Date(Date.now())
     }).json({
